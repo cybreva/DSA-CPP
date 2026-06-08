ad235fd008a6264dc0ce1b14d37cd4363f11b334
Tanmay <mtvkhanna@gmail.com>
2026-06-08 12:21:04 +0530

    for loop practice

--- loops.cpp
+++ loops.cpp
@@ -10,17 +10,37 @@ int main (){
     
 
     //for loop 
+
+    //printing even numbers
     for (int i = 0 ; i<=13; i = i+2 ){
          
-        std :: cout << i << ". hello world \n";
+        cout << i << ". hello world \n";
          
         
     }
+
+    //printing odd numbers
     for (int i =1 ; i<=13; i = i+2 ){
          
-        std :: cout << i << ". hello world \n";
+        cout << i << ". hello world \n";
          
         
+    }
+    //printing dot-line pattern (inverted)
+    for (int line = 13 ; line >=1;line--){
+
+        for (int dot = 1; dot <= line; dot++) {
+            cout << ".";
+        }
+        cout <<  "\n";
+    }
+    //printing dot-line pattern
+    for (int line = 1 ; line <=13;line++){
+
+        for (int dot = 1; dot <= line; dot++) {
+            cout << ".";
+        }
+        cout <<  "\n";
     }
 
 
