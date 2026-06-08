d5649ee2aebfdff179f1293649974c46d039d77f
Tanmay <mtvkhanna@gmail.com>
2026-06-08 11:58:33 +0530

    updated gitignore

--- loops.cpp
+++ loops.cpp
@@ -4,15 +4,23 @@
 
 using namespace std ;
 
+
 int main (){
     
-    string name = "world" ;
+    
 
     //for loop 
-    for (int i =0 ; i<=12; i++){
+    for (int i = 0 ; i<=13; i = i+2 ){
          
+        std :: cout << i << ". hello world \n";
          
-        cout << "hello" << name << "\n";
+        
+    }
+    for (int i =1 ; i<=13; i = i+2 ){
+         
+        std :: cout << i << ". hello world \n";
+         
+        
     }
 
 
