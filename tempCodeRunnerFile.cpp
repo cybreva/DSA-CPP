26ac810c60b0eb32af33242ac2fd3e5c9ca25fce
Tanmay <mtvkhanna@gmail.com>
2026-06-08 12:48:31 +0530

    removed unncecary files

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-//printing dot-line pattern (inverted)
-    for (int line = 13 ; line >=1;line--){
-
-        for (int dot = 1; dot <= line; dot++) {
-            cout << ".";
-        }
-        cout <<  "\n";
-    }
