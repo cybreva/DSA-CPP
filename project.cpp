1c723afef624e46664a9923110f067b9ac1343da
Tanmay <mtvkhanna@gmail.com>
2026-06-10 12:10:52 +0530

    bank sytem project

--- project.cpp
+++ project.cpp
@@ -1,16 +1,80 @@
 #include <iostream>
-#include<string>
+#include <string>
 
-using namespace std ;
+using namespace std;
 
-string name_input (){
-
-    string name ;
-        cout << "\nenter name : ";
-        cin >> name ;
+void welcome() {
+    cout << "========WELCOME=========\n===========CITY BANK=========\n";
 }
 
-int age_input (){
+string name_input() {
+
+    string name;
+
+    cout << "\nENTER YOUR NAME : ";
+    cin >> name;
+
+    return name;
+}
+
+int acc_number_input() {
+
+    int account_number;
+
+    cout << "\nPLEASE ENTER YOUR ACCOUNT NUMBER : ";
+    cin >> account_number;
+
+    return account_number;
+}
+int pin_account (){
+    
+    int pin_account;
+    
+    cout << " please enter your pin ";
+    
+    cin >> pin_account ;
+    
+    return pin_account ;
+}
+
+int main() {
+
+    welcome();
+
+    string name = name_input();
+
+    int account_number = acc_number_input();
+
+
+    int pin ;
 
     
+   do{  
+    
+        pin = pin_account() ;
+
+        cout << "  PLEASE ENTER YOUR PIN  ";
+        cin >> pin ;
+
+        if(pin != 7221)
+        {
+            cout << "Wrong PIN! Try Again.\n";
+        }
+
+    }
+    while (pin != 7221);
+
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
