915562d53fc63b95ffdf6f6c91c8fcd935b11b2a
Tanmay <mtvkhanna@gmail.com>
2026-06-13 10:43:49 +0530

    searching in vector

---
+++ vectors2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std ;
+
+int main(){
+
+    vector<int> numbers;
+
+    numbers.push_back(10);
+    numbers.push_back(20);
+    numbers.push_back(30);
+    numbers.push_back(40);
+    numbers.push_back(50);
+//numbers = {10,20,30,40,50}
+    int target;
+
+    cout << "Enter Number To Search: ";
+    cin >> target;
+
+    bool found = false;
+
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        if(numbers[i] == target)
+        {
+            found = true;
+            break;
+        }
+    }
+
+
+    if(found)
+    {
+        cout << "Number Found";
+    }
+    else
+    {
+        cout << "Number Not Found";
+    }
+
+
+}
