3c1b78ae798a8d20f58c8372888a92584cc6ee1a
Tanmay <mtvkhanna@gmail.com>
2026-06-17 11:31:47 +0530

    vectors

--- STL_STARTING/vector4.cpp
+++ STL_STARTING/vector4.cpp
@@ -4,20 +4,25 @@
 using namespace std;
 
 void add_number(vector <int> &x){
-    int temp ;
+    int temp ; 
+    for (int i = 0 ; i < 5 ; i++){    
     cout << "enter the number you want to add " << endl;
     cin >> temp ;
     x.push_back(temp);
+    }
+    
 }
 void display(const vector <int> &y){
     for (int i = 0 ; i < y.size(); i++){
         cout << y[i] << "\n" ;
 
     }
+    cout << y.size();
 }
 int main (){
-    vector<int> nums = {10,20,30};
-
+    vector<int> nums ;
+    
     add_number(nums);
 
-    display(nums);}
+    display(nums);
+}
