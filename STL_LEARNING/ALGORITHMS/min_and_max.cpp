b03a9986e7b0b5a4e0c0e6535d6911c16f068312
Tanmay <mtvkhanna@gmail.com>
2026-06-21 14:40:47 +0530

    practiced min_element and max_element

---
+++ STL_LEARNING/ALGORITHMS/min_and_max.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector> 
+#include <algorithm>
+
+using namespace std; 
+
+/*1. min_element()
+2. max_element()
+3. value print karo
+4. index print karo
+5. duplicate min/max test karo*/
+
+void minelement(vector <int> &nums){
+    auto it = min_element(
+        nums.begin() ,
+        nums.end() 
+    ) ;
+
+    cout << "\nthe minimum value in the vector is : " << *it << " and its index is : " << it-nums.begin() << "\n" << flush;
+}
+
+void maxelement(vector <int> &nums){
+    auto it = max_element(
+        nums.begin() ,
+        nums.end() 
+    );
+    
+    cout << "\nthe maximum value in the vector is : " << *it << " and its index is : " << it-nums.begin() << "\n" << flush;
+}
+
+int main(){
+    ios::sync_with_stdio(false) ;
+    cin.tie(nullptr);
+
+    vector <int> x = {10, 100, 123, 4321, 5432,123, 534,10 ,1 , 1343, 21,42,3,2,1} ;
+    minelement(x);
+    maxelement(x);
+    
+}
