276c96a47006ccc72d72edc27ab8e316ffcd6936
Tanmay <mtvkhanna@gmail.com>
2026-07-01 11:33:35 +0530

    fixed some bugs to avoid assertion /core dump

--- STL_LEARNING/containers/deque.cpp
+++ STL_LEARNING/containers/deque.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 #include <deque>
+#include <algorithm>
 
 using namespace std;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    deque<int> x;
+    deque<int> dq;
 
-    x.push_front(100000);
-    x.push_back(1212);
+    dq.push_front(100000);
+    dq.push_back(1212);
+    cout << dq.size() << flush ;
+    dq.pop_front();
+    dq.pop_front();
+    cout << dq.size() << flush;
+    dq.push_front(100000);
+    dq.push_back(1212);
+    for_each(
+        dq.begin(),
+        dq.end(),
+        [](int n){
+            cout << n << " " << flush ;
+        }
+    );
 
-    
+    cout << endl;
+
+    cout << dq[1] << flush;
+    cout << " \n" << flush ;
+    cout << dq.at(1) << flush;
+    cout  << " \n" << flush ;
+
 }
