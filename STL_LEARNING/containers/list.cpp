ebb092467cb890a67b8c633275bd556afaeb3d22
Tanmay <mtvkhanna@gmail.com>
2026-07-02 13:52:18 +0530

    covered std::list

--- STL_LEARNING/containers/list.cpp
+++ STL_LEARNING/containers/list.cpp
@@ -6,13 +6,14 @@
 using namespace std;
 
 int main(){
-    list<int> ls ;
+    list<int> ls = {1,21,32,23,432,23,32,432,43,2,34,42,32,4323,24234,};
 
-
+    cout << "actuall list = {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" <<flush ;
+    
     auto it = ls.begin();
     it = ls.insert(it,99);
-    
-
     cout << "after insertion list = {" << flush ;
     for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
     cout << "}\n" <<flush ;
@@ -42,6 +43,34 @@ int main(){
     
     cout << "random access : " << flush ;
     advance(it, 2);
-    
+    advance(it, 2);
+    cout << *it << "\n" << flush;
 
+    ls.sort();
+    cout << "after sorting list : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
+
+    ls.reverse();
+    cout << "after reversing list : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
+
+    ls.unique();
+    cout << "after running ls.unique(); list : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
+
+    list<int> ls2 = {12,123,321,54,4567,2354,7657823,346,654};
+    cout << "before merging list ls : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
+    ls.merge(ls2);
+    cout << "after merging list ls and list ls2  : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
+    ls.splice(ls.begin() , ls2);
+    cout << "after running ls.splice(ls2); list will be  : {" << flush ;
+    for_each(ls.begin(),ls.end(),[](int n){cout << n << " " << flush ;});
+    cout << "}\n" << flush ;
 }
