ce432a9d22d86693ce1bcafcc528f19479c33ca3
Tanmay <mtvkhanna@gmail.com>
2026-07-04 12:54:53 +0530

    starting : sets

---
+++ STL_LEARNING/containers/set.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){
+    set<int> st ;
+    //declaration
+    st.insert(10);
+    st.insert(3);
+    st.insert(7);
+    st.insert(10);
+    st.insert(3);
+
+    //fucntions
+    
+   
+
+
+}
