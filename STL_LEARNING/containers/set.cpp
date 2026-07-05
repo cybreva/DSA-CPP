22b3728f3bec3ffb6d5bbafa84457d4bb9a420d0
Tanmay <mtvkhanna@gmail.com>
2026-07-05 14:56:29 +0530

    learned set

--- STL_LEARNING/containers/set.cpp
+++ STL_LEARNING/containers/set.cpp
@@ -5,12 +5,38 @@ using namespace std;
 
 int main(){
     set<int> st ;
-    //declaration
+    multiset<int> mst;
+    //declaration of set
     st.insert(10);
     st.insert(3);
     st.insert(7);
     st.insert(10);
     st.insert(3);
+    st.insert(2321);
+    st.insert(31312);
+    st.insert(343225);
+    st.insert(354);
+    st.insert(3465);
+    st.insert(334);
+    st.insert(33765);
+    st.insert(33567);
+    st.insert(3357);
+    st.insert(33765);
+    st.insert(3365);
+    st.insert(33765);
+    st.insert(33765);
+    st.insert(33765);
+    st.insert(3354343);
+    st.insert(33765);
+    st.insert(354765);
+    st.insert(33455);
+
+    //display a fucntion
+     cout << "Set elements: ";
+    for (auto x : st)
+        cout << x << " ";
+
+    cout << "\n";
 
     //fucntions
     
