0e1281bbae20c34be52142f77505f5c9004928cc
Tanmay <mtvkhanna@gmail.com>
2026-07-08 09:28:45 +0530

    learned : map

--- STL_LEARNING/containers/map.cpp
+++ STL_LEARNING/containers/map.cpp
@@ -1,38 +1,29 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
 
     map<int,string> students;
 
-    students[100] = "tanmay";
-    students[133] = "rahul";
-    students.insert({121 , "amit"});
-    students.insert({131 , "ravi"});
+    int roll_number;
+    string name;
 
-    for(auto value : students){
-        cout << value.first << " : " << value.second << "\n" << flush;
+    for(int i=1;i<=3;i++){
+
+        cout<<"Enter roll number "<<i<<": ";
+        cin>>roll_number;
+
+        cin.ignore();
+
+        cout<<"Enter name "<<i<<": ";
+        getline(cin,name);
+
+        students[roll_number]=name;
     }
 
-    auto it = students.find(121);
 
-    if(it != students.end())
-    {
-        cout << it->second << flush;
+    for(auto value: students){
+        cout<<value.first<<" : "<<value.second<<"\n";
     }
-    cout << "\n" << flush;
-
-    cout << students.count(121);
-    students.erase(121);
-    cout << "\n" << flush;
-    for(auto value : students){
-        cout << value.first << " : " << value.second << "\n" << flush;
-    }
-
-    cout << "\n" << flush;
-
 
 }
