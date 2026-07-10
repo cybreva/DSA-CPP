ec24024104db1baac181084cb1bb687c2a8f2763
Tanmay <mtvkhanna@gmail.com>
2026-07-10 10:43:51 +0530

    finded mimimum number in an aarray

--- DSA_LEARNING/DSA CORE /ARRAYS/max_array.cpp
+++ DSA_LEARNING/DSA CORE /ARRAYS/max_array.cpp
@@ -7,12 +7,17 @@ int main(){
     int arr[5] = {1,2,3,4,5} ; 
     size_t n = sizeof(arr)/sizeof(arr[0]);
     int maximum = arr[0];
-    
+    int minimum = maximum ;
     for(int i = 0 ; i < n ; i++){
-        if(max < arr[i]){
-            max = arr[i];
+        if(maximum < arr[i]){
+            maximum = arr[i];
+        }
+        if(minimum > arr[i]){
+            minimum = arr[i];
         }
     }
     cout << "the maximum number is : " << maximum << "\n" <<  flush ;
+    
+    cout << "the minimum number is : " << minimum << "\n" <<  flush ;
 
 }
