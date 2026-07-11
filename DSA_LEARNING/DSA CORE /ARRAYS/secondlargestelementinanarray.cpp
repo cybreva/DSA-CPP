b46558436a4572f24359ed6dfb111fbd495b3c55
Tanmay <mtvkhanna@gmail.com>
2026-07-11 11:45:57 +0530

    learned : second largest element using single traversal

--- DSA_LEARNING/DSA CORE /ARRAYS/secondlargestelementinanarray.cpp
+++ DSA_LEARNING/DSA CORE /ARRAYS/secondlargestelementinanarray.cpp
@@ -4,29 +4,21 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int arr[10] = {1,2,3,4,5,45,354,34,534,34} ; 
+    int arr[] = {100, 50, 90}; 
     size_t n = sizeof(arr)/sizeof(arr[0]);
-    int maximum = arr[0];
-    int minimum = maximum ;
-    int secondLargest = arr[0] ;
-    for(int i = 0 ; i < n ; i++){
-        
-        if (arr[i] > maximum)
-        {
-            secondLargest = maximum;
-            maximum = arr[i];
+    int Largest = arr[0];
+    int secondLargest = INT_MIN ;
+    for(size_t i = 1 ; i < n ; i++){
+        if(Largest < arr[i]){
+            secondLargest = Largest ;
+            Largest = arr[i];
         }
-        else if (arr[i] > secondLargest)
-        {
-            secondLargest = arr[i];
+        else if ( arr[i] > secondLargest && arr[i] < Largest){
+            secondLargest = arr[i] ;
         }
-        
     }
-
-     cout << "the maximum number is : " << maximum << "\n" <<  flush ;
-    cout << "the second largest number is : " << secondLargest << "\n" << flush ;
-    
-
+   
+    cout << " the largest number is  : '"  << Largest << "'  \n"  << " and the second largest number is : '" << secondLargest << "' \n" << flush ;
 }
 
 
