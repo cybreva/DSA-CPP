85ca1eb37ab2efec46c6c8b6cfd144b6ab873aef
Tanmay <mtvkhanna@gmail.com>
2026-07-14 09:22:30 +0530

    learn(arrays): implemented left rotation by k positions (brute force)

---
+++ DSA_LEARNING/DSA CORE /ARRAYS/left_rotate_by_k.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int arr[] = {1,2,3,4,5};
+    size_t n = sizeof(arr)/sizeof(arr[0]); 
+    int k ;
+    cout << "enter the value of k : " << flush ;
+    cin >> k ;
+    int temp  ;
+    for (int j = 0; j < k; j++)
+    {
+        temp = arr[0];
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[n - 1] = temp;
+    }
+    
+
+    
+    
+    for(size_t  i = 0 ; i < n ; i++) {
+        cout << arr[i] << " " << flush ;
+    }
+    
+    cout << "\n" ;
+
+
+}
