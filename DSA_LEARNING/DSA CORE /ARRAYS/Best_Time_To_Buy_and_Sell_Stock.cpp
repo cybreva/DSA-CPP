c0d8e8dd1e046ad5566f90798b62a84d3ffcde30
Tanmay <mtvkhanna@gmail.com>
2026-07-16 09:36:04 +0530

    some more questionesd on array

---
+++ DSA_LEARNING/DSA CORE /ARRAYS/Best_Time_To_Buy_and_Sell_Stock.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int StockPrice[] = {7,1,5,3,6,4} ;
+    size_t n = sizeof(StockPrice)/sizeof(StockPrice[0]) ;
+    int MaxProfit = 0 ;
+    for(int i = 0 ; i < n ; i ++)
+    {
+        for(int j = i+1 ; j < n ; j++)
+        {
+            if( (StockPrice[j] - StockPrice[i]) > MaxProfit )
+            {
+               MaxProfit = StockPrice[j] - StockPrice[i] ;
+            }
+            
+        }
+    }
+    
+}
---
+++ DSA_LEARNING/DSA CORE /ARRAYS/greatest_sum_of_subrarray.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int arr[] = {1,2,3,4,5} ;
+    size_t n = sizeof(arr)/sizeof(arr[0]) ;
+    
+}
